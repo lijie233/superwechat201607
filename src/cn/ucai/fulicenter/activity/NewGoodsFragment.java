6e302232ac52e6ae03e15e6f3e84a9ca18b7defe
lijie <771706409@qq.com>
2016-08-02 10:01:10 +0800

    新品fragment的初始化布局

--- src/cn/ucai/fulicenter/activity/NewGoodsFragment.java
+++ src/cn/ucai/fulicenter/activity/NewGoodsFragment.java
@@ -1,9 +1,56 @@
 package cn.ucai.fulicenter.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import cn.ucai.I;
+import cn.ucai.bean.NewGoodBean;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.adapter.GoodsAdapter;
 
 /**
  * Created by Administrator on 2016/8/1.
  */
 public class NewGoodsFragment extends Fragment{
+    FuliCenterMainActivity mContext;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView mRecyclerView;
+    GridLayoutManager mGridLayoutManager;
+    GoodsAdapter mAdapter;
+    List<NewGoodBean> mGoodList;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mContext = (FuliCenterMainActivity) getContext();
+        View layout = View.inflate(mContext, R.layout.fragment_new_good, null);
+        initView(layout);
+        return layout;
+    }
+
+    private void initView(View layout) {
+        mSwipeRefreshLayout = (SwipeRefreshLayout) layout.findViewById(R.id.srl_new_good);
+        mSwipeRefreshLayout.setColorSchemeColors(
+                R.color.google_blue,
+                R.color.google_yellow,
+                R.color.google_red,
+                R.color.google_green
+        );
+        mRecyclerView = (RecyclerView) layout.findViewById(R.id.rv_new_good);
+        mGridLayoutManager = new GridLayoutManager(mContext, I.COLUM_NUM);
+        mGridLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        mAdapter = new GoodsAdapter(mContext, mGoodList);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
 }
