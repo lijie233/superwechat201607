b06d294f9239351e18809913f824112cda3a1388
lijie <771706409@qq.com>
2016-08-03 11:38:19 +0800

    初始化商品详情布局文件

--- src/cn/ucai/fulicenter/adapter/GoodsAdapter.java
+++ src/cn/ucai/fulicenter/adapter/GoodsAdapter.java
@@ -1,6 +1,7 @@
 package cn.ucai.fulicenter.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +16,11 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import cn.ucai.D;
 import cn.ucai.I;
 import cn.ucai.bean.NewGoodBean;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.activity.GoodDetailsActivity;
 import cn.ucai.fulicenter.utils.ImageUtils;
 
 /**
@@ -66,11 +69,18 @@ public class GoodsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         if (holder instanceof GoodViewHolder){
             mGoodViewHolder = (GoodViewHolder) holder;
-            NewGoodBean good = mGoodList.get(position);
+            final NewGoodBean good = mGoodList.get(position);
             ImageUtils.setGoodThumb(mContext,mGoodViewHolder.ivGoodThumb,good.getGoodsThumb());
             // mGoodViewHolder.ivGoodThumb.setImageURI(good.getGoodsThumb());
             mGoodViewHolder.tvGoodName.setText(good.getGoodsName());
             mGoodViewHolder.tvGoodPrice.setText(good.getCurrencyPrice());
+            mGoodViewHolder.layoutGood.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mContext.startActivity(new Intent(mContext, GoodDetailsActivity.class).putExtra(D.GoodDetails.KEY_GOODS_ID, good.getGoodsId()));
+
+                }
+            });
         }
         if (holder instanceof FooterViewHolder) {
             mFooterViewHolder= (FooterViewHolder) holder;
