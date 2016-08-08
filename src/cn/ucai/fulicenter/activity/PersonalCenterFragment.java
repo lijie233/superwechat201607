7348c4cd1acca1c07cde197e0b5e85ead5ce388d
lijie <771706409@qq.com>
2016-08-08 11:12:11 +0800

    添加SettingsActivity

--- src/cn/ucai/fulicenter/activity/PersonalCenterFragment.java
+++ src/cn/ucai/fulicenter/activity/PersonalCenterFragment.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulicenter.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -16,7 +17,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import cn.ucai.bean.NewGoodBean;
+import cn.ucai.fulicenter.DemoHXSDKHelper;
 import cn.ucai.fulicenter.R;
 
 /**
@@ -37,8 +38,30 @@ public class PersonalCenterFragment extends Fragment {
         mContext = (FuliCenterMainActivity) getContext();
         View layout = View.inflate(mContext, R.layout.fragment_personal_center, null);
         initView(layout);
+        setListener();
         return layout;
     }
+
+    private void setListener() {
+        MyClickListener listener = new MyClickListener();
+        mtvSettings.setOnClickListener(listener);
+        layoutUserCenter.setOnClickListener(listener);
+    }
+
+    class MyClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if (DemoHXSDKHelper.getInstance().isLogined()) {
+                switch (view.getId()) {
+                    case R.id.tv_center_settings:
+                    case R.id.center_user_info:
+                        startActivity(new Intent(mContext,SettingsActivity.class));
+                    break;
+                }
+            }
+        }
+    }
+
 
     private void initView(View layout) {
         mivUserAvatar = (ImageView) layout.findViewById(R.id.iv_user_avatar);
